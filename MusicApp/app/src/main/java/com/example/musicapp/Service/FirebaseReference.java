35a96ce21f2010f802533c6e866712e86fd45eef
DynamoVue <truongvuetify@gmail.com>
2021-07-16 08:47:39 +0700

    resolve conflict

--- MusicApp/app/src/main/java/com/example/musicapp/Service/FirebaseReference.java
+++ MusicApp/app/src/main/java/com/example/musicapp/Service/FirebaseReference.java
@@ -8,11 +8,8 @@ import com.google.firebase.storage.StorageReference;
 public interface FirebaseReference {
     DatabaseReference DATABASE_REFERENCE_MUSIC = FirebaseDatabase.getInstance().getReference("music");
     DatabaseReference DATABASE_REFERENCE_PLAYLIST = FirebaseDatabase.getInstance().getReference("playlist");
-<<<<<<< HEAD
-=======
     DatabaseReference DATABASE_REFERENCE_ALBUM = FirebaseDatabase.getInstance().getReference("albums");
     DatabaseReference DATABASE_REFERENCE_USERS = FirebaseDatabase.getInstance().getReference("users");
->>>>>>> a2974ca6f5a1946b68138c407b95de2c91553508
     FirebaseStorage storage = FirebaseStorage.getInstance();
     // [START download_create_reference]
     // Create a storage reference from our app
